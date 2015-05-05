ebe2572d7ae525ace8c59edb4bffd2134d1aa8d0
Frank Christiaan Peelen <frank.peelen@yahoo.com>
2015-05-05 15:45:15 +0900

    Set up a real MySQL Database

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
 	host: 'localhost',
 	user: 'root',
-	password: 'password'
+	password: 'password',
+	database: 'node'
 });
 
 connection.query(
-	'SELECT "foo" AS first_field, "bar" AS second_field',
+	'SELECT id, content FROM test',
 	function (err, results, fields) {
 		if (err) {
 			console.log('A database error occured!');
